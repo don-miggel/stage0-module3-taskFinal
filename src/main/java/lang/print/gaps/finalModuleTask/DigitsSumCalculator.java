045ba3066404ef184cc6db3faee47428dbdebf2d
Mykhailo Derecha <don.miggel@yahoo.com>
2023-02-18 20:08:14 +0200

    Module 3, final task is done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int step = number;
+        int number1 = number % 10;
+        step /= 10;
+        int number2 = step % 10;
+        step /=10;
+        int number3 = step % 10;
+        step /=10;
+        int number4 = step % 10;
+
+        System.out.println(number1+number2+number3+ number4);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+
+        int number1 = number % 10;
+        int step = number / 10;
+        int number2 = step % 10;
+        step /=10;
+        int number3 = step % 10;
+
+        System.out.println(number1+""+number2+""+number3);
+
     }
 }
